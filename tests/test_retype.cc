aa61eaaef22035db76dc89bd8568290c807f3b18
Martin Schulze <teebaum@cvs.gnome.org>
2003-11-01 14:55:23 +0000

    - Use a shorter notation for ..._mem_function variants. - Change order of
    2003-11-01  Martin Schulze  <teebaum@cvs.gnome.org>
    
    	* sigc++/[class_slot,method_slot,object_slot,hide].h.m4,
    	sigc++/signal.h.m4, sigc++/functors/mem_fun.h.m4:
    	- Use a shorter notation for ..._mem_function variants.
    	- Change order of mem_funcotr<>'s template arguments to
    	match std::mem_fun_t and to be more consistent with adaptors.
    	- Use ::sigc::slot's implicit ctor in compatibility module.
    	* sigc++/adaptors/lambda/operator.h.m4: Change some
    	lambda action names to match action names in std.
    
    	API addition:
    	* sigc++/adaptors/retype.h.m4: New file adding
    	adaptor retype.
    	* sigc++/Makefile.am: Build and distribute new file.
    	* tests/test_retype.cc: New file testing adaptor retype.
    	* MSVC_Net2003/tests/test_retype/test_reytype.vcproj,
    	tests/Makefile.am: Build and distribute new test case.

---
+++ tests/test_retype.cc
@@ -0,0 +1,36 @@
+// -*- c++ -*-
+/* Copyright 2002, The libsigc++ Development Team
+ *  Assigned to public domain.  Use as you wish without restriction.
+ */
+
+#include <sigc++/adaptors/retype.h>
+#include <iostream>
+
+struct foo : public sigc::trackable
+{
+  float test_int(int i) 
+    {std::cout << "foo(int "<<i<<")"<<std::endl; return i*1.5f;}
+  float test_float(float f) 
+    {std::cout << "foo(float "<<f<<")"<<std::endl; return f*5;}
+};
+
+void bar(short s)
+  {std::cout << "bar(short "<<s<<")"<<std::endl;}
+
+int main()
+{
+  foo foo_;
+  std::cout << sigc::retype(sigc::mem_fun(foo_, &foo::test_int))(1.234f) << std::endl;
+  std::cout << sigc::retype(sigc::mem_fun(foo_, &foo::test_float))(5) << std::endl;
+  sigc::retype(sigc::ptr_fun(&bar))(6.789f);
+
+  sigc::slot<float,float> s1 = sigc::retype(sigc::mem_fun(foo_, &foo::test_int));
+  sigc::slot<float,int>   s2 = sigc::retype(sigc::mem_fun(foo_, &foo::test_float));
+  sigc::slot<void,double> s3 = sigc::retype(sigc::ptr_fun(&bar));
+  std::cout << s1(1.234f) << std::endl;
+  std::cout << s2(5) << std::endl;
+  s3(6.789);
+
+  s2 = sigc::retype(s1);
+  std::cout << s2(5) << std::endl;
+}
