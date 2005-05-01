9d3a58d8953932e3619da68ee2b31d8aff5d1f98
Murray Cumming <murrayc@src.gnome.org>
2005-05-01 01:37:08 +0000

    Added test

---
+++ tests/test_copy_invalid_slot.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sigc++/sigc++.h>
+
+void Foo(sigc::trackable &t) {}
+
+int main(int argc, char **argv)
+{
+   sigc::trackable *t = new sigc::trackable();
+   std::cout << "sigc::trackable instance at " << t << std::endl;
+   sigc::slot<void> foo = sigc::bind(sigc::ptr_fun(Foo), sigc::ref(*t));
+   // This invalidates foo.
+   delete t;
+
+   // Try to pollute the memory previously occupied by the sigc::trackable
+   // instance. The hope is that with a regular memory allocator (i.e. not
+   // valgrind), we end up with buffer == (void *)t.
+   void *buffer = malloc(sizeof(sigc::trackable));
+   memset(buffer, 0xFF, sizeof(sigc::trackable));
+   std::cout << "         Polluted buffer at " << buffer << std::endl;
+
+   // Now copy foo: up to libsigc++ version 2.0.11, the copy constructor fails
+   // because the pointer value it dereferences does not point to a
+   // sigc::trackable anymore, it now points to a polluted buffer.
+   sigc::slot<void> bar = foo;
+
+   free(buffer);
+   return 0;
+}
