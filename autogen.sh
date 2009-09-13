9b07543a7a88c53dc9e2194d493a40061ef4e9a6
Daniel Elstner <daniel.kitta@gmail.com>
2009-09-13 16:45:41 +0200

    Enable verbose output of autoreconf
    * autogen.sh: Pass --verbose option to autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,6 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
-mm-common-prepare --force --copy "$srcdir"
-autoreconf --force --install "$srcdir"
+mm-common-prepare --copy --force "$srcdir"
+autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
