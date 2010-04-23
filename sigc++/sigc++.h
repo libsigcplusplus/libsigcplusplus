a35d1d92bda4a1fbbd23ed9221a6d729cef7dc33
David King <davidk@openismus.com>
2010-04-23 12:09:46 +0200

    Add main page to Doxygen documentation
    * docs/Makefile.am: Parse sigc++/sigc++.h for documentation.
    * sigc++/sigc++.h: Add main page to Doxygen documentation.

--- sigc++/sigc++.h
+++ sigc++/sigc++.h
@@ -20,6 +20,59 @@
 #ifndef SIGCXX_SIGCXX_H
 #define SIGCXX_SIGCXX_H
 
+/** @mainpage libsigc++ Reference Manual
+ *
+ * @section description Description
+ *
+ * libsigc++ implements a typesafe callback system for standard C++. It allows
+ * you to define signals and to connect those signals to any callback function,
+ * either a global or a member function, regardless of whether it is static or
+ * virtual. It also contains adaptor classes for connection of dissimilar
+ * callbacks and has an ease of use unmatched by other C++ callback libraries.
+ *
+ * For instance, see @ref adaptors "Adaptors", @ref sigcfunctors "Functors",
+ * @ref lambdas "Lambdas", @ref signal "Signals" and @ref slot "Slots".
+ *
+ * See also the libsigc++ website: http://libsigc.sourceforge.net/
+ *
+ * @section features Features
+ *
+ * - Compile time typesafe callbacks (faster than run time checks)
+ * - Typesafety violations report line number correctly with template names (no
+ *   tracing template failures into headers)
+ * - No compiler extensions or meta compilers required
+ * - Proper handling of dynamic objects and signals (deleted objects will not
+ *   cause seg faults)
+ * - Extendable API at any level: signal, slot, connection and trackable
+ * - Extensions do not require alteration of basic components
+ * - User definable accumulators
+ * - A variety of adaptors to change the callback signature: bind, hide,
+ *   retype, compose and lambda call groups
+ *
+ * @section basics Basic usage
+ *
+ * Include the libsigc++ header:
+ * @code
+ * #include <sigc++/sigc++.h>
+ * @endcode
+ * This includes every header installed by libsigc++, so can slow down
+ * compilation, but suffices for this simple example. Assuming that your
+ * program source file is @c program.cc, compile it with:
+ * @code
+ * g++ program.cc -o program `pkg-config --cflags --libs sigc++-2.0`
+ * @endcode
+ * Alternatively, if using autoconf, use the following in @c configure.ac:
+ * @code
+ * PKG_CHECK_MODULES([LIBSIGCXX], [sigc++-2.0])
+ * @endcode
+ * Then use the generated @c LIBSIGCXX_CFLAGS and @c LIBSIGCXX_LIBS variables
+ * in the project @c Makefile.am files. For example:
+ * @code
+ * program_CPPFLAGS = $(LIBSIGCXX_CFLAGS)
+ * program_LDADD = $(LIBSIGCXX_LIBS)
+ * @endcode
+ */
+
 #include <sigc++/signal.h>
 #include <sigc++/connection.h>
 #include <sigc++/trackable.h>
@@ -27,4 +80,3 @@
 #include <sigc++/functors/functors.h>
 
 #endif /* SIGCXX_SIGCXX_H */
-
