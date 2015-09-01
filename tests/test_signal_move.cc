85ce9bcf95e8d1603eadded16451c329b4aad1ee
Murray Cumming <murrayc@murrayc.com>
2015-09-01 16:24:36 +0200

    Added test_signal_move.
    This seems to work.

---
+++ tests/test_signal_move.cc
@@ -0,0 +1,50 @@
+// -*- c++ -*-
+/* Copyright 2015, The libsigc++ Development Team
+ *  Assigned to public domain.  Use as you wish without restriction.
+ */
+
+#include "testutilities.h"
+#include <sigc++/trackable.h>
+#include <sigc++/signal.h>
+#include <sigc++/functors/ptr_fun.h>
+#include <sigc++/functors/mem_fun.h>
+#include <string>
+#include <cstdlib>
+
+namespace
+{
+std::ostringstream result_stream;
+
+int foo(int i)
+{
+  result_stream << "foo(int " << i << ")";
+  return 1;
+}
+
+} // end anonymous namespace
+
+int main(int argc, char* argv[])
+{
+  auto util = TestUtilities::get_instance();
+
+  if (!util->check_command_args(argc, argv))
+    return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  // signal
+  sigc::signal<int, int> sig;
+  sig.connect(sigc::ptr_fun(&foo));
+  sig(1);
+  util->check_result(result_stream, "foo(int 1)");
+
+  //Test the move constructor:
+  sigc::signal<int, int> sig2(std::move(sig));
+  sig2(2);
+  util->check_result(result_stream, "foo(int 2)");
+
+  //Test the move assignment operator:
+  sigc::signal<int, int> sig3 = std::move(sig2);
+  sig3(3);
+  util->check_result(result_stream, "foo(int 3)");
+
+  return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
