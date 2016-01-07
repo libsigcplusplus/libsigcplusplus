65dab995f4a6fe7fb4c18d3276816ee568be10da
Murray Cumming <murrayc@murrayc.com>
2016-01-07 19:51:17 +0100

    bind_return.h: Use this as a normal .h file.
    Instead of generating it from a .h.m4 file.

--- sigc++/adaptors/bind_return.h
+++ sigc++/adaptors/bind_return.h
@@ -1,26 +1,5 @@
-dnl Copyright 2002, The libsigc++ Development Team
-dnl
-dnl This library is free software; you can redistribute it and/or
-dnl modify it under the terms of the GNU Lesser General Public
-dnl License as published by the Free Software Foundation; either
-dnl version 2.1 of the License, or (at your option) any later version.
-dnl
-dnl This library is distributed in the hope that it will be useful,
-dnl but WITHOUT ANY WARRANTY; without even the implied warranty of
-dnl MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-dnl Lesser General Public License for more details.
-dnl
-dnl You should have received a copy of the GNU Lesser General Public
-dnl License along with this library; if not, write to the Free Software
-dnl Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-dnl
-divert(-1)
-
-include(template.macros.m4)
-
-
-divert(0)dnl
-_FIREWALL([ADAPTORS_BIND_RETURN])
+#ifndef _SIGC_ADAPTORS_BIND_RETURN_H_
+#define _SIGC_ADAPTORS_BIND_RETURN_H_
 #include <sigc++/adaptors/adaptor_trait.h>
 #include <sigc++/adaptors/bound_argument.h>
 
@@ -74,7 +53,7 @@ struct bind_return_functor : public adapts<T_functor>
    * @param _A_functor Functor to invoke from operator()().
    * @param _A_ret_value Value to return from operator()().
    */
-  bind_return_functor(_R_(T_functor) _A_functor, _R_(T_return) _A_ret_value)
+  bind_return_functor(type_trait_take_t<T_functor> _A_functor, type_trait_take_t<T_return> _A_ret_value)
     : adapts<T_functor>(_A_functor), ret_value_(_A_ret_value)
     {}
 
@@ -121,3 +100,4 @@ bind_return(const T_functor& _A_functor, T_return _A_ret_value)
 { return bind_return_functor<T_return, T_functor>(_A_functor, _A_ret_value); }
 
 } /* namespace sigc */
+#endif /* _SIGC_ADAPTORS_BIND_RETURN_H_ */
