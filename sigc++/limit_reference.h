796ba33ff178e1f0daf03ea7951dd51134f84a1a
Murray Cumming <murrayc@murrayc.com>
2016-03-05 11:54:53 +0100

    use limit_reference.h as a regular .h file.
    Instead of generating it.

--- sigc++/limit_reference.h
+++ sigc++/limit_reference.h
@@ -1,26 +1,5 @@
-dnl Copyright 2002, The libsigc++ Development Team
-dnl
-dnl This library is free software; you can redistribute it and/or
-dnl modify it under the terms of the GNU Lesser General Public
-dnl License as published by the Free Software Foundation; either
-dnl version 2.1 of the License, or (at your option) any later version.
-dnl
-dnl This library is distributed in the hope that it will be useful,
-dnl but WITHOUT ANY WARRANTY; without even the implied warranty of
-dnl MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-dnl Lesser General Public License for more details.
-dnl
-dnl You should have received a copy of the GNU Lesser General Public
-dnl License along with this library; if not, write to the Free Software
-dnl Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-dnl
-divert(-1)
-
-include(template.macros.m4)
-
-divert(0)
-
-_FIREWALL([LIMIT_REFERENCE])
+#ifndef _SIGC_LIMIT_REFERENCE_H_
+#define _SIGC_LIMIT_REFERENCE_H_
 
 #include <sigc++/visit_each.h>
 #include <sigc++/type_traits.h>
@@ -128,8 +107,8 @@ private:
 };
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-/** Implementation of visitor specialized for the [$1]limit_reference
- * class, to call visit_each() on the entity returned by the [$1]limit_reference's
+/** Implementation of visitor specialized for the $1limit_reference
+ * class, to call visit_each() on the entity returned by the $1limit_reference's
  * visit() method.
  * @tparam T_type The type of the reference
  * @tparam T_action The type of functor to invoke.
@@ -150,3 +129,4 @@ struct visitor<limit_reference<T_type> >
 
 } /* namespace sigc */
 
+#endif /* _SIGC_LIMIT_REFERENCE_H_ */
