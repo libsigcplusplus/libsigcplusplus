109409f3087ae6e2bd446554be53b1faea43bc40
Murray Cumming <murrayc@murrayc.com>
2016-03-11 10:36:41 +0100

    test_signal(): Restructure this.
    To make it clearer and to keep the small tests more self-contained and
    separate.

--- tests/test_signal.cc
+++ tests/test_signal.cc
@@ -13,17 +13,13 @@
 
 namespace
 {
+
+TestUtilities* util = nullptr;
 std::ostringstream result_stream;
 
 int foo(int i)
 {
   result_stream << "foo(int " << i << ") ";
-  return 1;
-}
-
-int bar(float i)
-{
-  result_stream << "bar(float " << i << ") ";
   return 1;
 }
 
@@ -42,21 +38,26 @@ struct A : public sigc::trackable
   }
 };
 
-} // end anonymous namespace
-
-int main(int argc, char* argv[])
+void test_empty_signal()
 {
-  auto util = TestUtilities::get_instance();
-
-  if (!util->check_command_args(argc, argv))
-    return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
-
   // signal
   sigc::signal<int,int> sig;
 
   // emit empty signal
   sig(0);
   util->check_result(result_stream, "");
+}
+
+int bar(float i)
+{
+  result_stream << "bar(float " << i << ") ";
+  return 1;
+}
+
+void test_auto_disconnection()
+{
+  // signal
+  sigc::signal<int,int> sig;
 
   // connect some slots before emitting & test auto-disconnection
   {
@@ -73,7 +74,10 @@ int main(int argc, char* argv[])
   sig(2);
   result_stream << sig.size();
   util->check_result(result_stream, "foo(int 2) bar(float 2) 2");
+}
 
+void test_reference()
+{
   // test reference
   A a;
   std::string str("guest book");
@@ -82,13 +86,35 @@ int main(int argc, char* argv[])
   sigstr(str);
   result_stream << str;
   util->check_result(result_stream, "A::foo(string 'guest book') foo was here");
+}
 
+void test_make_slot()
+{
   // test make_slot()
+  sigc::signal<int,int> sig;
   sig.connect(sigc::ptr_fun1(&foo));
+  sig.connect(sigc::ptr_fun(&bar));
+  sig.connect(sigc::ptr_fun(&foo));
+
   sigc::signal<int,int> sig2;
   sig2.connect(sig.make_slot());
   sig2(3);
   util->check_result(result_stream, "foo(int 3) bar(float 3) foo(int 3) ");
+}
+
+} // end anonymous namespace
+
+int main(int argc, char* argv[])
+{
+  util = TestUtilities::get_instance();
+
+  if (!util->check_command_args(argc, argv))
+    return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  test_empty_signal();
+  test_auto_disconnection();
+  test_reference();
+  test_make_slot();
 
   return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
