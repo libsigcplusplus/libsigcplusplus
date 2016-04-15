d20175a4829ea1bf0f847576f03790c5b5e43abe
Murray Cumming <murrayc@murrayc.com>
2016-04-15 11:03:29 +0200

    visit_each_type: More comments.

--- sigc++/visit_each.h
+++ sigc++/visit_each.h
@@ -194,7 +194,13 @@ visit_each(const T_action& _A_action, const T_functor& _A_functor)
  *
  * It is currently used only to call slot_do_bind and slot_do_unbind
  * only on trackable-derived functors, like a compile-time version of
- * if(dynamic_cast<trackable*)(&functor) { slot_do_unbind(functor); }
+ *   if(dynamic_cast<trackable*)(&functor) { slot_do_unbind(functor); }
+ * This also depends on do_visit_each() method overloads for
+ * limit_derived_target< , slot_do_bind/slot_do_unbind> parameters
+ * in the visitor<slot> template specialization.
+ * TODO: Remove the need for slot_do_bind/slot_do_unbind, limit_derived_target,
+ * and visit_each_type() by just using a constexpr_if
+ * (previously known as static_if) if that ends up in C++17.
  *
  * @ingroup sigcfunctors
  */
