0fc9e6e83bd451f29f337496a8bf893fb1756920
Murray Cumming <murrayc@murrayc.com>
2016-04-20 09:05:05 +0200

    Added missing copyright headers.
    These files might be older than 2003, but that's all I see in our
    git respository (imported from the old svn repository, which probably
    lost some history once).

--- sigc++/adaptors/adapts.h
+++ sigc++/adaptors/adapts.h
@@ -1,5 +1,24 @@
+/*
+ * Copyright 2003 - 2016, The libsigc++ Development Team
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #ifndef SIGC_ADAPTORS_ADAPTS_H
 #define SIGC_ADAPTORS_ADAPTS_H
+
 #include <sigc++config.h> //To get SIGC_TEMPLATE_KEYWORD_OPERATOR_OVERLOAD
 #include <sigc++/visit_each.h>
 #include <sigc++/functors/functor_trait.h>
