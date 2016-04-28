57a20ea1f4b64c82e2c6c4711619393415e88af1
Murray Cumming <murrayc@murrayc.com>
2016-04-28 11:03:26 +0200

    Added sigc::internal::weak_raw_ptr<>.
    As a simpler way to null a pointer to an object when that object
    is deleted.

---
+++ sigc++/weak_raw_ptr.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016, The libsigc++ Development Team
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#ifndef SIGC_WEAK_RAW_PTR_HPP
+#define SIGC_WEAK_RAW_PTR_HPP
+#include <sigc++/trackable.h> //Just for notifiable.
+
+namespace sigc
+{
+
+struct notifiable;
+
+namespace internal
+{
+
+/** T must derive from sigc::trackable.
+ */
+template <typename T>
+struct weak_raw_ptr : public sigc::notifiable
+{
+  inline weak_raw_ptr()
+  : p_(nullptr)
+  {}
+  
+  inline weak_raw_ptr(T* p) noexcept
+  : p_(p)
+  {
+    if(!p)
+      return;
+      
+    p->add_destroy_notify_callback(this, &notify_object_invalidated);
+  }
+  
+  inline weak_raw_ptr(const weak_raw_ptr& src) noexcept
+  : p_(src.p_)
+  {
+    p_->add_destroy_notify_callback(this, &notify_object_invalidated);
+  }
+  
+  inline weak_raw_ptr& operator=(const weak_raw_ptr& src) noexcept
+  {
+    if(p_) {
+      p_->remove_destroy_notify_callback(this);
+    }
+
+    p_ = src.p_;
+    p_->add_destroy_notify_callback(this, &notify_object_invalidated);
+
+    return *this;
+  }
+
+  //TODO:
+  weak_raw_ptr(weak_raw_ptr&& src) = delete;
+  weak_raw_ptr& operator=(weak_raw_ptr&& src) = delete;
+  
+  inline ~weak_raw_ptr() noexcept
+  {
+    if (p_) {
+      p_->remove_destroy_notify_callback(this);
+    }
+  }
+  
+  inline explicit operator bool() const noexcept
+  {
+    return p_ != nullptr;
+  }
+
+  inline T* operator->() const noexcept
+  {
+    return p_;
+  }
+
+private:
+  /** Callback that is executed when the objet is destroyed.
+   * @param data The object notified (@p this).
+   */
+  static void notify_object_invalidated(notifiable* data)
+  {
+    weak_raw_ptr* self = static_cast<weak_raw_ptr*>(data);
+    if(!self)
+      return;
+
+    self->p_ = nullptr;
+  }
+  
+  T* p_;
+};
+
+} /* namespace internal */
+
+} /* namespace sigc */
+
+#endif /* SIGC_WEAK_RAW_PTR_HPP */
---
+++ tests/test_weak_raw_ptr.cc
@@ -0,0 +1,73 @@
+/* Copyright 2016, The libsigc++ Development Team
+ *  Assigned to public domain.  Use as you wish without restriction.
+ */
+
+#include "testutilities.h"
+#include <sigc++/weak_raw_ptr.h>
+#include <cassert>
+
+namespace
+{
+
+TestUtilities* util = nullptr;
+std::ostringstream result_stream;
+
+class A : public sigc::trackable
+{
+public:
+  void something()
+  {
+    result_stream << "method called";
+  }
+};
+
+} // end anonymous namespace
+
+void test_weak_ptr_becomes_null()
+{
+  const auto a = new A();
+  sigc::internal::weak_raw_ptr<A> raw_ptr(a);
+  assert(raw_ptr);
+
+  //Call something on A, via the weak_raw_ptr<>,
+  //just to make sure that it doesn't get optimised away:
+  raw_ptr->something();
+  util->check_result(result_stream, "method called");
+
+  delete a;
+
+  //Deleting the A should have made the weak_raw_ptr<A> become invalid.
+  assert(!raw_ptr);
+}
+
+void test_weak_ptr_disconnects_self()
+{
+  const auto a = new A();
+  {
+    sigc::internal::weak_raw_ptr<A> raw_ptr(a);
+
+    //Call something on A, via the weak_raw_ptr<>,
+    //just to make sure that it doesn't get optimised away:
+    raw_ptr->something();
+    util->check_result(result_stream, "method called");
+  }
+
+  //If the weak_raw_ptr has not asked A to stop notifying it,
+  //then we would expect some undefined behaviour here,
+  //when a tries to notify the now-destroyed weak_raw_ptr.
+  delete a;
+}
+
+int
+main(int argc, char* argv[])
+{
+  util = TestUtilities::get_instance();
+
+  if (!util->check_command_args(argc, argv))
+    return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  test_weak_ptr_becomes_null();
+  test_weak_ptr_disconnects_self();
+
+  return util->get_result_and_delete_instance() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
