4e75b93462f68b7a2870130b7e8ce03b1207ce46
Murray Cumming <murrayc@murrayc.com>
2020-04-25 10:03:03 +0200

    run make format, to clang-format the code
    This was with clang-format-9

--- examples/qt_with_qmake/exampleclass.h
+++ examples/qt_with_qmake/exampleclass.h
@@ -9,28 +9,28 @@
 
 class ExampleClass : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit ExampleClass(QObject *parent = nullptr);
+  explicit ExampleClass(QObject* parent = nullptr);
 
-/* Instead of using the keyword 'signals', use the 'Q_SIGNALS' macro */
+  /* Instead of using the keyword 'signals', use the 'Q_SIGNALS' macro */
 Q_SIGNALS:
-    void example_signal();
+  void example_signal();
 
-/* Instead of using the keyword 'slots', use the 'Q_SLOTS' macro */
+  /* Instead of using the keyword 'slots', use the 'Q_SLOTS' macro */
 public Q_SLOTS:
-    void timer_slot();
+  void timer_slot();
 
-    /**
-     * This slot is called using libsigc++, however since it is defined under Q_SLOTS
-     * it could also be used with the Qt signals/slots
-     */
-    void example_slot();
+  /**
+   * This slot is called using libsigc++, however since it is defined under Q_SLOTS
+   * it could also be used with the Qt signals/slots
+   */
+  void example_slot();
 
 private:
-    sigc::slot<void()> m_sigc_slot;
-    sigc::signal<void()> m_sigc_signal;
-    QTimer m_timer;
+  sigc::slot<void()> m_sigc_slot;
+  sigc::signal<void()> m_sigc_signal;
+  QTimer m_timer;
 };
 
 #endif // EXAMPLECLASS_H
