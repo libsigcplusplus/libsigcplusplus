497f6e49a2477ac4681a88681814ff08ca718419
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2020-10-12 13:12:10 +0200

    sigc++/sigc++.h: Describe how to use libsigc++ with meson

--- sigc++/sigc++.h
+++ sigc++/sigc++.h
@@ -64,6 +64,25 @@
  * @code
  * g++ program.cc -o program `pkg-config --cflags --libs sigc++-2.0`
  * @endcode
+ * If your version of g++ is not C++11-compliant be default,
+ * add the @c -std=c++11 option.
+ *
+ * @subsection meson Using Meson
+ *
+ * If using <a href="https://mesonbuild.com/">Meson</a>, include the following
+ * in @c meson.build:
+ * @code
+ * sigc_dep = dependency('sigc++-2.0')
+ * program_name = 'program'
+ * cpp_sources = [ 'program.cc' ]
+ * executable(program_name,
+ *   cpp_sources,
+ *   dependencies: sigc_dep
+ * )
+ * @endcode
+ *
+ * Your @c dependencies: keyword argument should also mention any other libraries
+ * that you need to use.
  *
  * @subsection autotools Using Autotools
  *
