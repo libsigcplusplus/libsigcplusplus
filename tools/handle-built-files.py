9e8a77a05e9de41abd227325727be8366a0af1cc
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2021-05-05 11:58:56 +0200

    Subprojects can use meson.add_dist_script() if meson.version() >= 0.58.0
    * meson.build:
    * MSVC_NMake/meson.build:
    * docs/manual/meson.build:
    * docs/reference/meson.build:
    * sigc++/meson.build:
    Call add_dist_script() in a subproject, if meson.version() >= 0.58.0.
    * tools/handle-built-files.py:
    * tools/tutorial-custom-cmd.py:
    Use MESON_PROJECT_DIST_ROOT if it exists, else MESON_DIST_ROOT.
    It exists if meson.version() >= 0.58.0.

--- tools/handle-built-files.py
+++ tools/handle-built-files.py
@@ -69,9 +69,12 @@ def dist_built_files(is_msvc_files=False):
   # <built_h_cc_dir> <dist_dir> <built_files>...
 
   # <built_h_cc_dir> is an absolute path in the build directory or source directory.
-  # <dist_dir> is a distribution directory, relative to MESON_DIST_ROOT.
+  # <dist_dir> is a distribution directory, relative to MESON_PROJECT_DIST_ROOT.
+
+  # MESON_PROJECT_DIST_ROOT is set only if meson.version() >= 0.58.0.
+  project_dist_root = os.getenv('MESON_PROJECT_DIST_ROOT', os.getenv('MESON_DIST_ROOT'))
   built_h_cc_dir = sys.argv[2]
-  dist_dir_root = os.path.join(os.getenv('MESON_DIST_ROOT'), sys.argv[3])
+  dist_dir_root = os.path.join(project_dist_root, sys.argv[3])
   dist_dir = dist_dir_root
 
   # Distribute .h and .cc files built from .m4 files, or generated MSVC files.
