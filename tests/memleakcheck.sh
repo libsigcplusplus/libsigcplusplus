44529fdebbab37504d27b5cf91f52678ecdeb13e
Slava Andrejev <vyac.andrejev@gmail.com>
2021-05-26 04:29:09 -0700

    Allow slots with rvalue reference parameters
    In code based on Glibmm it's quite common to convert a Glib C structure
    to a Glib::Object's descendant before passing it as a parameter to a
    slot. If the slot is not supposed to modify the object, then everything
    is fine, we can declare the parameter as a const reference and the
    temporary object will be accurately dealt with by the compiler. However,
    if the Glib based object is getting modified, the only way to pass it to
    the slot is to declare it a Glib::RefPtr shared pointer. This creates a
    lot of unnecessary churn of memory allocations for a simple deal of
    calling a signal. However, C++ has a specific mean for this particular
    semantic of passing a temporary object: rvalue reference.
    
    For example, somewhere inside Gtkmm might be a declaration:
    _WRAP_SIGNAL(void sun_rose(const Glib::RefPtr<Gtk::Sun> &new_sun),
    "sun-rose")
    
    Instead its more semantically correct to write:
    _WRAP_SIGNAL(void sun_rose(Gtk::Sun &&new_sun), "sun-rose")
    
    And later somewhere in your code:
    world->signal_sun_rose().connect([&](auto &&new_sun) {
      new_sun.shine();
    });
    
    This commit makes a couple of simple modifications that allow declaring
    signals and slots with rvalue reference parameter.

--- tests/memleakcheck.sh
+++ tests/memleakcheck.sh
@@ -9,8 +9,8 @@ for testprog in  test_accum_iter test_accumulated test_bind test_bind_as_slot \
   test_copy_invalid_slot test_cpp11_lambda test_custom test_disconnect \
   test_disconnect_during_emit test_exception_catch test_hide \
   test_limit_reference test_member_method_trait test_mem_fun test_ptr_fun \
-  test_retype test_retype_return test_signal test_signal_move test_size \
-  test_slot test_slot_disconnect test_slot_move test_trackable \
+  test_retype test_retype_return test_rvalue_ref test_signal test_signal_move \
+  test_size test_slot test_slot_disconnect test_slot_move test_trackable \
   test_trackable_move test_track_obj test_tuple_cdr test_tuple_end \
   test_tuple_for_each test_tuple_start test_tuple_transform_each \
   test_visit_each test_visit_each_trackable test_weak_raw_ptr
